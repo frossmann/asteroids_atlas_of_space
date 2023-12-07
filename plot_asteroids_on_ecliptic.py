0aed7c0e2080bf16e239df016869bc4888902b33
Francis Rossmann <frossmann@eoas.ubc.ca>
2023-12-07 14:59:03 -0800

    add legend

--- plot_asteroids_on_ecliptic.py
+++ plot_asteroids_on_ecliptic.py
@@ -56,8 +56,8 @@ planets_to_plot = ["Earth", "Mars", "Jupiter"]
 
 
 classes = list(asteroids.keys())
-# classes_to_exclude = ["TNO", "AST", "CEN"]
-classes_to_exclude = []
+classes_to_exclude = ["TNO", "AST", "CEN", "MCA"]
+
 
 cmags = np.linspace(0, 0.8, len(classes) - len(classes_to_exclude))
 asteroid_colors = [cmap(cmag) for cmag in cmags]
@@ -128,11 +128,26 @@ ax.scatter(
     alpha=0.5,
     s=69,
 )
-# plt.legend(bbox_to_anchor=(1, 1))
+legend = plt.legend(
+    frameon=False, ncols=4, loc="lower center", bbox_to_anchor=[0.5, -0.08]
+)
 ax.set_rlim(0, 1e9)
 ax.axis("off")
+for handle in legend.legendHandles[4:]:
+    handle._sizes = [20]
 
-plt.savefig("asteroid_belt_ecliptic_v3.pdf", bbox_inches="tight")
+
+# plt.savefig("asteroid_belt_ecliptic_v4.pdf", bbox_inches="tight")
 #
+
+# %%
+count = 0
+for ii, the_class in enumerate(classes):
+    if the_class in classes_to_exclude:
+        continue
+
+    the_bodies = asteroids[the_class]
+
+    count += the_bodies.shape[0]
 
 # %%
